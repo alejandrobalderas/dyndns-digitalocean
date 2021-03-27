9b506ddf94cb6eca3e27f4ab3faae1a52d97bc59
Alejandro Balderas <alejandro@hotmail.de>
2021-03-27 10:27:11 +0100

    Endless loop with 1 minute sleep. Code refactored

--- main.py
+++ main.py
@@ -1,44 +1,61 @@
 import requests
 import os
 import json
+import time
+from typing import List
 
-domain = "balderas.de"
-subdomain = "*"
 apikey = os.environ["apikey"]
 
-url = f"https://api.digitalocean.com/v2/domains/{domain}/records"
+last_ip = current_ip = "0.0.0.0"
+current_ip = requests.get("http://ifconfig.me").text
+
+domains = {
+    "balderas.de": [
+        "*",
+        "manage"
+    ],
+    "balderas-home.com": [
+        "example",
+        "example2"
+    ]
+}
+
 headers = {
     "Authorization": f"Bearer {apikey}",
     "Content-Type": "application/json"
-    }
-params = {"type": "A", "name": f"{subdomain}.{domain}"}
-
-r = requests.get(url, headers=headers, params=params)
-record_id = r.json()["domain_records"][0]["id"]
-record_ip = r.json()["domain_records"][0]["data"]
-record_url = url + f"/{record_id}"
-
-current_ip = requests.get("https://ifconfig.me").text
-
-if current_ip != record_ip:
-    update_ip = {"data": f"{current_ip}"}
-    r = requests.put(record_url, headers=headers, data=json.dumps(update_ip))
-else:
-    print("No change made")
+}
+    
+params = {
+    "type": "A",
+}
 
 
-class Dyndns:
+def get_record_ids(do_url:str, subdomains:List[str]) -> List[str]:
+    response = requests.get(do_url, headers=headers, params=params)
+    response = response.json()["domain_records"]
+    return [record["id"] for record in response if record["name"] in subdomains]
 
-    def __init__(self, domain:str, subdomain:str = None, apikey:str=None) -> None:
-        self.url = f"https://api.digitalocean.com/v2/domains/{self.domain}/records"
-        self.headers = {
-            "Authorization": f"Bearer {apikey}",
-            "Content-Type": "application/json"
-            }
-        self.record_id = None
-        self.current_ip = None
+def get_record_urls(domain:str, subdomains:List[str]) -> List[str]:
+    do_url = f"https://api.digitalocean.com/v2/domains/{domain}/records"   
+    record_ids = get_record_ids(do_url, subdomains)
+    return [do_url + f"/{record_id}" for record_id in record_ids]    
 
-    def get_record_info(self):
-        params = {"type": "A", "name": f"{subdomain}.{domain}"}
-        r = requests.get(url, headers=headers, params=params)
-        self.record_id = r.json()["domain_records"][0]["id"]
+def update_record_ids(record_urls:List[str], new_ip: str) -> None:
+    new_ip = {"data": f"{new_ip}", "ttl": 60}
+    for record_url in record_urls:
+        r = requests.put(record_url, headers=headers, data=json.dumps(new_ip))
+        if r.status_code == 200:
+            print(f"Changed record for {record_url} to IP {new_ip['data']}")
+
+while True:
+
+    if current_ip != last_ip:
+        print("Changing IPs")
+        domain_urls = {}
+        for domain, subdomains in domains.items():
+            record_urls = get_record_urls(domain, subdomains)        
+            update_record_ids(record_urls, current_ip)
+        last_ip = current_ip
+    time.sleep(60)
+
+
